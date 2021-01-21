d48af558418e08920d999c174ab79379278d5306
tanzhang666 <18717735973@163.com>
2021-01-21 19:19:02 +0800

    first commit

---
+++ demo-consumer/src/main/java/com/learn/springboot/DemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConsumerApplication.class, args);
+    }
+
+}
